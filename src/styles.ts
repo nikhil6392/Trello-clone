d344375fd7828eee3471b4bc7186fcf3d0377917
nikhil6392 <nikhilpathak210431@gmail.com>
2024-10-19 04:24:52 +0530

    Defining the layout of the app

---
+++ src/styles.ts
@@ -0,0 +1,33 @@
+import styled from "styled-components"
+
+export const AppContainer=styled.div`
+    align-items:flex-start;
+    background-color:#643e66;
+    display:flex;
+    flex-direction:row;
+    height:100%;
+    padding:20px;
+    width:100%;
+`
+export const ColumnContainer=styled.div`
+    background-color:#f27a8a;
+    width:300px;
+    min-height:40px;
+    margin-right:20px;
+    border-radius:3px;
+    padding:8px 8px;
+    flew-grow:0;
+`
+export const ColumnTitle=styled.div`
+    padding:6px 20px 12px;
+    font-weight:bold;
+`
+export const CardContainer=styled.div`
+    background-color:#c9f6a0;
+    cursor:pointer;
+    margin-bottom:0.5rem;
+    padding:0.5rem 1rem;
+    max-width:300px;
+    border-radius:3px;
+    box-shadow:#091e4240 0px 1px 0px 0px;
+    `
